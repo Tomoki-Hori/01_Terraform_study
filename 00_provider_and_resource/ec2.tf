07b8d9dc48697d6444e62ce8b9de546d96f05579
Tomoki-Hori <tomo.horinouchi0611+github@gmail.com>
2024-11-24 07:28:19 +0900

    add security group.ids

--- 00_provider_and_resource/ec2.tf
+++ 00_provider_and_resource/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0b20f552f63953f0e"
-  instance_type = "t2.micro"
+  ami                    = "ami-0b20f552f63953f0e"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data                   = <<-EOF
             #! /bin/bash
