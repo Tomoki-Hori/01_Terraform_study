9a55e862896d837642f1d700595058f49981c9ed
Tomoki-Hori <tomo.horinouchi0611+github@gmail.com>
2024-11-24 06:29:07 +0900

    add_user_data

--- 00_provider_and_resource/ec2.tf
+++ 00_provider_and_resource/ec2.tf
@@ -2,6 +2,12 @@ resource "aws_instance" "example" {
   ami           = "ami-0b20f552f63953f0e"
   instance_type = "t2.micro"
 
+user_data = <<-EOF
+            #! /bin/bash
+            echo "Hello World" > index.html
+            nohup bydybox httpd -f -p &
+            EOF
+
   tags = {
     Name = "terraform-example"
   }
