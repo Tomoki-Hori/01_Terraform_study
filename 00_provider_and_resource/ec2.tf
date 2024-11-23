7933f351396f832ff214c23a17d5213228699216
Tomoki-Hori <tomo.horinouchi0611+github@gmail.com>
2024-11-24 07:36:49 +0900

    edit ec2.tf

--- 00_provider_and_resource/ec2.tf
+++ 00_provider_and_resource/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   user_data                   = <<-EOF
             #! /bin/bash
             echo "Hello World" > index.html
-            nohup bydybox httpd -f -p &
+            nohup busybox httpd -f -p &
             EOF
   user_data_replace_on_change = true
 
