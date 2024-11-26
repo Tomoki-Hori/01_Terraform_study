27a8a36253fce0eb6219b140373c34fdd7f9dcdd
Tomoki-Hori <tomo.horinouchi0611+github@gmail.com>
2024-11-26 22:38:54 +0900

    edit ec2.tf

--- 00_provider_and_resource/ec2.tf
+++ 00_provider_and_resource/ec2.tf
@@ -3,11 +3,11 @@ resource "aws_instance" "example" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data                   = <<-EOF
-            #! /bin/bash
-            echo "Hello World" > index.html
-            nohup busybox httpd -f -p ${var.server_port} &
-            EOF
+  user_data = <<-EOF
+              #! /bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
   user_data_replace_on_change = true
 
   tags = {
