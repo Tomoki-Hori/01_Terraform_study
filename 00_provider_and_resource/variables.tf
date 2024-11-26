5bc43836e893b7f2a98479251c0224ff2c0eee90
Tomoki-Hori <tomo.horinouchi0611+github@gmail.com>
2024-11-27 08:43:43 +0900

    modify some files

--- 00_provider_and_resource/variables.tf
+++ 00_provider_and_resource/variables.tf
@@ -1,5 +1,5 @@
-#variable "number_example" {
-#    description = "An example of a number variable in Terraform"
-#    type = number
-#    default = 42
-#}
+variable "number_example" {
+    description = "An example of a number variable in Terraform"
+    type = number
+    default = 42
+}
